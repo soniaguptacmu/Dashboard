4964b3669c474f05f3dc9ed292dd5718372a4ce1
Renfei Song <renfei.song@gmail.com>
2017-06-19 12:31:34 -0400

    login ui

---
+++ mysite/nalanda/static/js/common.js
@@ -0,0 +1,27 @@
+if (typeof toastr !== 'undefined') {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+}
+
+$(document).ready(function() {
+    var maxVal = function(a, b) {
+        return a > b ? a : b;
+    };
+    var resizeSiteContent = function() {
+        $('.site-content')[0].style.minHeight = maxVal($('.site-sidebar').height(), $(window).height() - 84) + 'px';
+    };
+    resizeSiteContent();
+    $(window).on('resize', resizeSiteContent);
+});
