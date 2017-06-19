6ed121ceea2e9644d92e63a48111f978e641a9f2
System Administrator <root@ruoyao-li.wv.cc.cmu.edu>
2017-06-19 13:26:08 -0400

    remove studio

--- studio/user_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from user_management import views
-
-urlpatterns = [
-  url(r'^login', views.login, name='login'),
-  url(r'^logout', views.logout, name='logout'),
-]
-
