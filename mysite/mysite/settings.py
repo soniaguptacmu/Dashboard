6a4e85d54b62852bb5d9b341cd5ad32806c9146a
https://github.com/17-654-MSE-Analysis/IoT-Group1 <soniag@andrew.cmu.edu>
2017-07-20 20:36:02 -0400

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nalanda',
     'USER': 'root',
-    'PASSWORD': 'Expect@1234',
+    'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT':''
     }
