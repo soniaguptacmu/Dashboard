bc12e94e01857a3046f72edc27136ee9a9b6efad
System Administrator <root@ruoyao-li.wv.cc.cmu.edu>
2017-07-21 15:43:26 -0400

    removed unneeded files

--- mysite/my/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/soniagupta/Desktop/sem3/studio/Visualizer/Kolibri-Visualizer/mysite/my/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
