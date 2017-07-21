6879e389e7c52a25ce72741a2478b305bf30f9e4
System Administrator <root@ruoyao-li.wv.cc.cmu.edu>
2017-07-21 18:30:39 -0400

    bug gix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nalanda',
     'USER': 'root',
-    'PASSWORD': '',
+    'PASSWORD': 'root',
     'HOST': '127.0.0.1',
     'PORT':''
     }
